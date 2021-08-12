e09fae743d54f062454cfa69ac89dc1439437de7
rishitkundu1 <85060575+rishitkundu1@users.noreply.github.com>
2021-08-12 19:49:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Badges Page code
+      // Badges modal code
+      var modalOpen = document.querySelector(".model-open");
+      var closeModelBtn = document.querySelector(".close");
+
+      function showModel(){
+            document.querySelector('.bg-modal').style.display = 'flex';
+      }
+      function closeModel(){
+            document.querySelector('.bg-modal').style.display = 'none';
+      }
+
+// Report card page Code
+      function showDownloadModel(){
+            document.querySelector('.download-bg-modal').style.display = 'block';
+      }
+      function closeDownloadModel(){
+            document.querySelector('.download-bg-modal').style.display = 'none';
+      }
